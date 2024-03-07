afe44d92183d7f59f8943d06914711d9cae50bf6
DaniAm94 <dani.amico94@gmail.com>
2024-03-07 17:39:12 +0100

    refactored home's template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,53 +11,22 @@
 </head>
 
 <body>
-    <header>
-        <div class="container">
 
-            <figure><img src="{{ asset('images/dc-logo.png') }}" alt="DC logo"></figure>
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#">
+    {{-- Header --}}
+    @include('includes.header')
 
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    </header>
+    {{-- Jumbotron --}}
+    @include('includes.jumbotron')
 
-    <footer>
-        <div class="footer-top">
-            <div class="container">
+    {{-- Main --}}
+    @include('includes.main')
 
-                <nav class="links-container">
-                    <div class="links-list">
-                    </div>
-                </nav>
-                <figure>
-                    <img src="{{ asset('images/dc-logo-bg.png') }}" alt="big dc logo">
-                </figure>
-            </div>
-        </div>
-        <div class="footer-bottom">
-            <div class="container">
-                <a href="#" id="register-btn">Sign-Up now!</a>
-                <nav>
-                    <ul>
-                        <li>
-                            <h3>Follow us</h3>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <img :src="" :alt="icon.text">
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
-    </footer>
+
+    {{-- Nav --}}
+    @include('includes.nav')
+
+    {{-- Footer --}}
+    @include('includes.footer')
 </body>
 
 </html>
